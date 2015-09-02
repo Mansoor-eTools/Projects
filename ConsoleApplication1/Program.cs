114caf367f06bd10e3bc3ef6a2a6b5c449f28352
mali325 <mali@e-tools.com.au>
2015-09-02 13:44:33 +1000

    ali

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,9 +7,13 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        static void maincopy()
+        {
+            Console.WriteLine("Hi");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+         
         }
     }
 }
